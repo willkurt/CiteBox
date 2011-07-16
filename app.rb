72678e38b102e345bf3e04f60851ad7e9ed533db
Will Kurt <wckurt@gmail.com>
2011-07-16 15:58:53 -0700

    starting on the basics of the ui

--- app.rb
+++ app.rb
@@ -1,6 +1,19 @@
 require 'sinatra'
+require 'erb'
+require 'json'
+require './citation'
+
 
 get '/' do
-  "Here we go!"
+  erb :index
 end
+
+#all the necessary config data is passed throught the url
+get '/citationtool/:host/:url' do 
+  content_type 'text/javascript'
+  @host = params[:host]
+  @url = params[:url]
+  erb :citationtool
+end
+
 
