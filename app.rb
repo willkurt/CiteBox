896c761426ba46d6e0390ef0e7b00916c6a79cc0
Will Kurt <wckurt@gmail.com>
2011-07-19 23:33:23 -0700

    added some mediocre handling for citations that aren't found

--- app.rb
+++ app.rb
@@ -22,12 +22,21 @@ post '/lookup' do
                         { :citation => citation_text})
   citation_xml = Hpricot(response.to_str)
   open_url_list = []
-  (citation_xml/'ctx:metadata journal')[0].each_child do |x|
-    open_url_list.push("#{x.name.gsub(':','.')}=#{x.innerHTML}")
+  citation_element = (citation_xml/'ctx:metadata journal')
+  if citation_element[0].nil?
+    redirect '/fail'
+  else
+    citation_element[0].each_child do |x|
+      open_url_list.push("#{x.name.gsub(':','.')}=#{x.innerHTML}")
+    end
+    open_url_query = open_url_list.join('&')
+    base = params[:open_url_base]
+    full_url = "http://#{base}/?query=#{open_url_query}&url_ver=Z39.88-2004&ctx_enc=info:ofi/enc:UTF-8"
+    puts "redircting to.. #{full_url}"
+    redirect full_url
   end
-  open_url_query = open_url_list.join('&')
-  base = params[:open_url_base]
-  full_url = "http://#{base}/?query=#{open_url_query}&url_ver=Z39.88-2004&ctx_enc=info:ofi/enc:UTF-8"
-  puts "redircting to.. #{full_url}"
-  redirect full_url
+end
+
+get '/fail' do
+  erb :fail
 end
